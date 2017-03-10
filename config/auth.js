29dd27b99e9ffe38d25956deed638a43b6e16a4c
samuliedvin <samuli.edvin@gmail.com>
2017-03-10 13:30:03 +0200

    trololoo

--- config/auth.js
+++ config/auth.js
@@ -3,13 +3,13 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '363415430724302', // your App ID
         'clientSecret'  : 'c1a70a18e87bc4166123316136e80938', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback/'
+        'callbackURL'   : 'http://lippuvaraus.herokuapp.com/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'uoNWZIus3YISuNEvhZ12WIRTr',
         'consumerSecret'    : 'LqkbnZ5oWnRcZl2QFtgtwKiuynr3HHfSzLtiMf6urmZgN0JMnU',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'       : 'http://lippuvaraus.herokuapp.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
